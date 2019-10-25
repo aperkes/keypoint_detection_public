d71632b8764fb4015e8688f3137f64c92da9048b
aperkes <perkes.ammon@gmail.com>
2019-10-25 17:44:17 -0400

    Make Bash exit clean
    Fixed batch_eval so that it doesn't
    -ignore SIGINTs
    -leave python running in the background, filling up the gpu

--- batch_eval.bash
+++ batch_eval.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap break INT
 
 echo $CONDA_DEFAULT_ENV
 FILES=/data/birds/postures/birdview2-2019/*wav.mp4
@@ -10,5 +11,5 @@ for f in $FILES
     do
         echo "Processing $f..."
         calib_path=$calib_dir$f"/calibration.yaml"
-        python /home/ammon/Documents/Scripts/keypoint_detection/eval.py --data_dir=$base_dir --video=$f --checkpoint=/home/ammon/Documents/Scripts/keypoint_detection/models/keypoint_model_checkpoint.pt --calib=$calib_path  
+        python /home/ammon/Documents/Scripts/keypoint_detection/eval.py --data_dir=$base_dir --video=$f --checkpoint=/home/ammon/Documents/Scripts/keypoint_detection/models/keypoint_model_checkpoint.pt --calib=$calib_path
     done
