f898460ce4754668c02367bd714b5bc5a3e324b0
aperkes <perkes.ammon@gmail.com>
2019-10-25 18:19:27 -0400

    Improving Batch
    Now it works with the calibration files...probably

--- batch_eval.bash
+++ batch_eval.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 trap break INT
 
-echo $CONDA_DEFAULT_ENV
 FILES=/data/birds/postures/birdview2-2019/*wav.mp4
 
 base_dir="/data/birds/postures/birdview2-2019/"
-calib_dir="/data/birds/postures/calibrations/birdview-2/calibrations"
+calib_dir="/data/birds/postures/calibrations/birdview-2/calibrations/"
 
 for f in $FILES
     do
         echo "Processing $f..."
-        calib_path=$calib_dir$f"/calibration.yaml"
+        file_name=$(basename "$f")
+        calib_path=$calib_dir$file_name"/calibration.yaml"
         python /home/ammon/Documents/Scripts/keypoint_detection/eval.py --data_dir=$base_dir --video=$f --checkpoint=/home/ammon/Documents/Scripts/keypoint_detection/models/keypoint_model_checkpoint.pt --calib=$calib_path
     done
